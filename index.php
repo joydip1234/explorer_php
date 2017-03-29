e27d7c78b517ac9f5d7cdc90343b99c247ac6939
Sudipta <sudipta.banik.112@gmail.com>
2017-03-29 13:21:09 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sudipta
+ * Date: 3/28/2017
+ * Time: 11:03 AM
+ */
+$count = 0;
+//$wr = fopen('files/new.txt', 'w');
+$toshow = true;
+$uploaded = [];
+if(!isset($_COOKIE["len"])){
+    setcookie("len",0, time() + (86400 * 30), "/");
+}
+if (!empty($_FILES["file"])) {
+    $uploaded = null;
+    $uploaded = array();
+    foreach ($_FILES["file"]["name"] as $key => $name) {
+        if ($_FILES['file']['error'][$key] == 0 && move_uploaded_file($_FILES['file']['tmp_name'][$key], "files/{$name}")) {
+            if(!in_array($name,$_COOKIE)){
+                $count++;
+                setcookie(crc32($name), $name, time() + (86400 * 30), "/");
+                $uploaded[$key] = $name;
+                //$toWrite = " \r\n[" . $key . "] " . $uploaded[$key] . "      --size " . round($_FILES['file']['size'][$key] / 1024) . "kB \r\n";
+                //fwrite($wr, $toWrite);
+            }else{
+                $uploaded[$key] = "already_here";
+            }
+        }
+
+        setcookie("len",$count, time() + (86400 * 30), "/");
+    }
+    //fclose($wr);
+    returnAjax($uploaded);
+}
+
+if(isset($_POST['deleteFiles'])){
+    array_map('unlink', glob("./files/*.*"));
+}
+
+
+
+function returnAjax($uploaded)
+{
+    if (!empty($_POST['ajax'])) {
+        die(json_encode($uploaded));
+    }
+}
+?>
+
+<html>
+<meta name="viewport" charset="utf-8">
+<link href="s.css" rel="stylesheet">
+<head><title>File Manager</title></head>
+<body>
+
+<!--?php
+echo "<div style='background-color: #7986cb;color: #EEE;padding: 10px;text-transform: uppercase;font-size: 0.95em;'>total uploaded files = <span id='count_holder'>";
+$c = 0;
+$files = glob('./files/*');if(!empty($files)){foreach($files as $file){if(is_file($file))$c++;}}
+
+echo $c."</span></div>";
+?-->
+<div id="progress_bar">
+
+</div>
+<div id="uploaded">
+    <span style="color: #fff;display: block;text-transform: uppercase;font-size: 0.7em;width: 100%;background-color: #7986cb;padding: 5px;box-sizing: border-box;">Uploaded Images</span>
+    <?php
+    /*$allowedExts = array("jpeg","jpg","svg","png","bmp","gif");
+    if (isset($_COOKIE)) {
+        foreach ($_COOKIE as $key => $value) {
+            $temp = explode(".",$value);
+            if($key != "len" && in_array(end($temp), $allowedExts)){
+                echo "<img class='shownfile' src='./files/" . $value . "' />";
+            }
+        }
+        $toshow = true;
+    }*/
+    $files = glob('./files/*'); // get all file names
+    if(!empty($files) || $toshow == true){
+        $allowedExts = ["jpeg","jpg","svg","png","bmp","gif"];
+        foreach($files as $file){
+            if(is_file($file))
+                $words  = explode("/",$file);
+                $temp = explode(".",$words[2]);
+                //print_r($file);
+                //print_r($temp);
+                if(in_array($temp[1],$allowedExts)){
+                    echo "<img class='shownfile' src='".$file."'/>";
+                }
+        }
+        $toshow = false;
+    }
+    ?>
+</div>
+<div style="background-color: #7986cb;color: #eee;width: 100%;padding: 15px;box-sizing: border-box">Files in Directory [
+    <?php
+    echo "<span id='count_holder'>";
+    $c = 0;
+    $files = glob('./files/*');if(!empty($files)){foreach($files as $file){if(is_file($file))$c++;}}
+    echo $c."</span>"
+    ?>
+] </div>
+<div id="folder">
+    <?php
+    $files = glob('./files/*'); // get all file names
+    if(!empty($files) || $toshow == true){
+        foreach($files as $file){ // iterate files
+            if(is_file($file))
+                $words  = explode("/",$file);
+            echo "<div class='file_cont'><a class='file_ls' href='".$file."'>".$words[2]."</a><span class='rm'> &#10005; </span></div>";
+        }
+        $tosshow = false;
+    }else{
+        echo "<p style='padding: 20px;text-align: center;width: 100%;' id='noFileToast'>No File in Directory</p>";
+    }
+    ?>
+</div>
+
+    <div id="fu">
+        <form action="index.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="file[]" value="" multiple="multiple" id="fileIn">
+            <input type="submit" value="upload" id="upload" style="margin-top: 5px;">
+            <!--<span style="clear: both;display: block;"></span>-->
+        </form>
+
+        <form>
+            <input type="button" value="clear cookies" name="clrck" id="clrck">
+            <input type="button" value="Delete Files" name="filedlt" id="filedlt">
+            <!--input type="button" value="Clear Log"  id="clearLog"-->
+        </form>
+
+    </div>
+
+
+<div id="log"><span id="log_header" style="padding: 10px;text-align: center;display: none;color: #eee;">Added Files</span> </div><br><br><br><br>
+<script src="main.js" type="text/javascript"></script>
+<!--<script src="jq.js" type="text/javascript"></script>-->
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,128 @@
+function $(e) {
+    return document.getElementById(e);
+}
+
+var handler = function (event) {
+    try {
+        event.preventDefault();
+        event.stopPropagation();
+        var fileIn = document.getElementById('fileIn');
+        var data = new FormData();
+        data.append('ajax', true);
+        for (var i = 0; i < fileIn.files.length; i++) {
+            //console.log(fileIn.files[i].name + " --> " + fileIn.files[i].size);
+            data.append('file[]', fileIn.files[i]);
+        }
+
+        var req = new XMLHttpRequest();
+
+        req.upload.addEventListener('progress', function (event) {
+            if (event.lengthComputable) {
+                var pr = event.loaded / event.total;
+                var progress = document.getElementById('progress_bar');
+                progress.innerHTML = "";
+                progress.style.opacity = "1";
+                progress.innerHTML = Math.round(pr * 100) + "%";
+            }
+
+        });
+
+        req.addEventListener('readystatechange', function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var resp = eval(this.response);
+                var x = $('uploaded');
+                var folder = $('folder');
+                //$('log_header').style.display = 'block';
+                for (var i = 0; i < resp.length; i++) {
+                    console.log(resp[i]);
+                    $('noFileToast').style.display = "none";
+                    if(resp[i]!="already_here"){
+                        var y = document.createElement("img");
+                        var div = document.createElement('div');
+                        div.classList.add('file_cont');
+                        var rm = document.createElement('span');
+                        rm.innerHTML = "&#10005;";
+                        rm.classList.add('rm');
+                        var link = document.createElement("a");
+                        link.setAttribute('href','./files/' + resp[i].toString());
+                        link.classList.add('file_ls');
+                        link.innerHTML = resp[i].toString();
+                        div.appendChild(link);
+                        div.appendChild(rm);
+                        folder.appendChild(div);
+                        y.setAttribute('src', './files/' + resp[i].toString());
+                        y.classList.add('shownfile');
+                        x.appendChild(y);
+                        //$("log").innerHTML += "<div class='filename'>" + resp[i] + "</div>";
+                        $('count_holder').innerHTML = parseInt($('count_holder').innerHTML) + 1;
+                    }else{
+                        $('progress_bar').innerHTML="same file can't be uploaded twice";
+                        $('progress_bar').style.opacity = "1"
+                        setTimeout(function () {
+                            $("progress_bar").style.opacity = '0';
+                        },1000);
+                    }
+
+                }
+
+            }
+
+        });
+
+        req.upload.addEventListener('load', function (e) {
+            $('progress_bar').style.opacity = '0';
+        });
+
+        req.upload.addEventListener('error', function () {
+            alert("error uploading file");
+        });
+
+        req.open('POST', 'index.php', true);
+        //req.setRequestHeader("Cache-Control", "no-cache");
+        req.send(data);
+    } catch (e) {
+        alert(e.which.toString());
+    }
+}
+function clearCookie() {
+    document.cookie.split(";").forEach(function (c) {
+        document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+    });
+    alert("all cookies cleared");
+    location.reload();
+}
+function clearDir() {
+    var dlt = new XMLHttpRequest();
+    var data = new FormData();
+    data.append("deleteFiles",true);
+    dlt.open("POST","index.php",true);
+    dlt.send(data);
+    document.cookie.split(";").forEach(function (c) {
+        document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+    });
+    location.reload();
+}
+window.addEventListener('load', function (event) {
+    var submit_btn = document.getElementById('upload');
+    submit_btn.addEventListener('click', handler);
+    $("clrck").addEventListener('click', clearCookie);
+    $("filedlt").addEventListener('click', clearDir);
+
+    /*$("clearLog").onclick= function () {
+        var l = document.getElementById("log");
+        while(l.hasChildNodes()){
+            l.removeChild(l.firstChild);
+        }
+    }*/
+});
+/**
+ * Created by Sudipta on 3/28/2017.
+ */
+
+
+
+
+
+
+
+
